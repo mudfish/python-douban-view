b80f84a7a6e0b6284784062b57d40f39e0a08fdf
zhihui <indexman@126.com>
2024-05-08 23:20:29 +0800

    update progress bar

--- main_async.py
+++ main_async.py
@@ -22,9 +22,25 @@ MOVIE_TYPES = ["剧情", "喜剧", "动作", "爱情"]
 # CSV文件名
 CSV_NAME = "movie_data.csv"
 # CSV头
-CSV_HEADS = ["id","title","year","directors","casts","rating","cover","country","summary","types","lang","release_date","time","url",]
+CSV_HEADS = [
+    "id",
+    "title",
+    "year",
+    "directors",
+    "casts",
+    "rating",
+    "cover",
+    "country",
+    "summary",
+    "types",
+    "lang",
+    "release_date",
+    "time",
+    "url",
+]
 
 engine = create_engine("mysql+pymysql://root:123456@127.0.0.1:3306/db_douban")
+
 
 class Spider:
     def __init__(self):
@@ -44,7 +60,7 @@ class Spider:
     def init(self):
         # 每次跑之前，先删除之前的csv文件
         if os.path.exists(CSV_NAME):
-            os.remove(CSV_NAME)    
+            os.remove(CSV_NAME)
         with open(CSV_NAME, "w", newline="", encoding="utf-8") as writer_f:
             writer = csv.writer(writer_f)
             writer.writerow(CSV_HEADS)
@@ -60,21 +76,23 @@ class Spider:
 
     async def get_movie_pages(self, session, type_name):
         start_page = self.page_progress.get(type_name, 1)
-        if(start_page<=MAX_PAGES):
-            for page in range(start_page, MAX_PAGES+1):
+        if start_page <= MAX_PAGES:
+            for page in range(start_page, MAX_PAGES + 1):
                 # print(f'{type_name}第{page}页：')
                 start_time = time.time()
-                params = {"start": (page - 1) * 20,"count":10, "tags": type_name}
+                params = {"start": (page - 1) * 20, "count": 10, "tags": type_name}
                 try:
-                    async with session.get(self.movie_page_url, headers=self.headers, params=params) as resp:
+                    async with session.get(
+                        self.movie_page_url, headers=self.headers, params=params
+                    ) as resp:
                         resp.raise_for_status()
                         respJson = await resp.json()
                         movie_list = respJson["items"]
-                        for i,m in enumerate(movie_list):
+                        for i, m in enumerate(movie_list):
                             if m["type"] == "movie":
-                                await self.process_movie(session,m)
-                                #progress_bar.update(round(1/len(movie_list)))
-                        self.page_progress[type_name] = page+1
+                                await self.process_movie(session, m)
+                                # progress_bar.update(round(1/len(movie_list)))
+                        self.page_progress[type_name] = page + 1
                         # 记录进度
                         self.save_page_progress()
                         # 刷新全局进度
@@ -82,15 +100,17 @@ class Spider:
                 except Exception as e:
                     print(f"处理:{type_name}第{page}页失败: {e}")
                     break
- 
+
     async def process_movie(self, session, movie):
         movie_data = []
         movie_data.append(movie["id"])
         movie_data.append(movie["title"])
         movie_data.append(movie["year"])
-        async with session.get(self.movie_detail_url.format(movie["id"]), headers=self.headers) as resp:
+        async with session.get(
+            self.movie_detail_url.format(movie["id"]), headers=self.headers
+        ) as resp:
             resp.raise_for_status()
-            html_text =  await resp.text()
+            html_text = await resp.text()
         path = etree.HTML(html_text)
         # 导演
         movie_data.append(",".join(path.xpath('//a[@rel="v:directedBy"]/text()')))
@@ -101,15 +121,27 @@ class Spider:
         # 封面
         movie_data.append(path.xpath('//img[@rel="v:image"]/@src')[0])
         # 国家
-        movie_data.append(path.xpath('//span[contains(text(),"制片国家")]/following-sibling::br[1]/preceding-sibling::text()[1]')[0])
+        movie_data.append(
+            path.xpath(
+                '//span[contains(text(),"制片国家")]/following-sibling::br[1]/preceding-sibling::text()[1]'
+            )[0]
+        )
         # 摘要
         movie_data.append(path.xpath('//span[@property="v:summary"]/text()')[0].strip())
         # 类型
-        movie_data.append(",".join(path.xpath('//div[@id="info"]/span[@property="v:genre"]/text()')))
+        movie_data.append(
+            ",".join(path.xpath('//div[@id="info"]/span[@property="v:genre"]/text()'))
+        )
         # 语言
-        movie_data.append(path.xpath('//span[contains(text(),"语言")]/following-sibling::br[1]/preceding-sibling::text()[1]')[0])
+        movie_data.append(
+            path.xpath(
+                '//span[contains(text(),"语言")]/following-sibling::br[1]/preceding-sibling::text()[1]'
+            )[0]
+        )
         # 上映日期
-        movie_data.append(path.xpath('//span[@property="v:initialReleaseDate"]/text()')[0][:10])
+        movie_data.append(
+            path.xpath('//span[@property="v:initialReleaseDate"]/text()')[0][:10]
+        )
         # 时长
         movie_data.append(path.xpath('//span[@property="v:runtime"]/text()')[0])
         # url
@@ -126,29 +158,38 @@ class Spider:
         df = pd.read_csv(CSV_NAME, encoding="utf-8")
         df.drop_duplicates(subset=["id"], keep="first", inplace=True)
         print("存储到数据库...")
-        df.to_sql("tb_movie", con=engine, index=False, if_exists="append") 
-        print('清理重复数据...')
-        engine.connect().execute(text("delete from tb_movie where id in (select id from (select id from tb_movie group by id having count(*) > 1) as t)"))
+        df.to_sql("tb_movie", con=engine, index=False, if_exists="append")
+        print("清理重复数据...")
+        engine.connect().execute(
+            text(
+                "delete from tb_movie where id in (select id from (select id from tb_movie group by id having count(*) > 1) as t)"
+            )
+        )
 
     def update_global_progress(self):
         self.completed_pages += 1
-        #print(self.completed_pages)
-        self.global_progress_bar.update(self.completed_pages)
+        # print(self.completed_pages)
+        self.global_progress_bar.update(1)
         self.global_progress_bar.refresh()
-        
+
     async def run(self):
         self.init()
         self.load_page_progress()
-        #self.total_pages = MAX_PAGES*len(MOVIE_TYPES) - sum(self.page_progress.get(type_name, 1) for type_name in MOVIE_TYPES)
+        # self.total_pages = MAX_PAGES*len(MOVIE_TYPES) - sum(self.page_progress.get(type_name, 1) for type_name in MOVIE_TYPES)
         for type_name in MOVIE_TYPES:
             if MAX_PAGES > self.page_progress.get(type_name, 1):
-                self.total_pages+=MAX_PAGES+1-self.page_progress.get(type_name, 1)
+                self.total_pages += MAX_PAGES + 1 - self.page_progress.get(type_name, 1)
         # print(self.total_pages)
         if self.total_pages > 0:
-            self.global_progress_bar = tqdm(total=self.total_pages, desc="progress", unit="page", colour='GREEN')
-            
+            self.global_progress_bar = tqdm(
+                total=self.total_pages, desc="progress", unit="page", colour="GREEN"
+            )
+
             async with aiohttp.ClientSession() as session:
-                tasks = [self.get_movie_pages(session, type_name) for type_name in self.movie_types]
+                tasks = [
+                    self.get_movie_pages(session, type_name)
+                    for type_name in self.movie_types
+                ]
                 await asyncio.gather(*tasks)
             # 请求结束后，清空页面进度
             # self.page_progress = {}
